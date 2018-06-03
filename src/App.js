dfd72402bd1a6a28a7e3b8c6dbf441bf328c420c
Onwuka Gideon <dongidomed@gmail.com>
2018-06-03 03:45:33 +0100

    Project structure and template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,106 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'mdbreact/dist/css/mdb.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, Mask, Row, Col, Fa, Button, View, Container, FormInline } from 'mdbreact';
+import Footer from './Components/Footer/index'
 
-class App extends Component {
-  render() {
+class AppPage extends React.Component {
+  constructor(props) {
+    super(props),
+    this.state = {
+      collapse : false
+    }
+    this.onClick = this.onClick.bind(this);
+    this.handleNavbarClick = this.handleNavbarClick.bind(this);
+  }
+
+  onClick(){
+    this.setState({
+        collapse: !this.state.collapse,
+    });
+  }
+
+  handleNavbarClick(){
+    this.setState({
+      collapse: false
+    });
+  }
+
+  render(){
+  const overlay = <div id="sidenav-overlay" style={{backgroundColor: 'transparent'}} onClick={this.handleNavbarClick}/>
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="apppage">
+        <Router>
+          <div>
+          <Navbar dark expand="md" fixed="top" scrolling>
+            <Container>
+              <NavbarBrand>
+                <strong className="white-text">MDB</strong>
+              </NavbarBrand>
+              <NavbarToggler onClick = { this.onClick } />
+              <Collapse isOpen = {this.state.collapse} navbar>
+                <NavbarNav left>
+                  <NavItem active>
+                      <NavLink to="#!">Home</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink to="#!">Link</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink to="#!">Profile</NavLink>
+                  </NavItem>
+                </NavbarNav>
+                <NavbarNav right >
+                  <NavItem>
+                    <FormInline waves>
+                      <div className="md-form my-0">
+                        <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+                      </div>
+                    </FormInline>
+                  </NavItem>
+                </NavbarNav>
+              </Collapse>
+            </Container>
+          </Navbar>
+          { this.state.collapse && overlay}
+          </div>
+        </Router>
+          <View>
+            <Mask className="d-flex justify-content-center align-items-center gradient">
+            <Container>
+              <Row>
+                <div className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5">
+                  <h1 className="h1-responsive font-weight-bold mt-sm-5">Make purchases with our app </h1>
+                  <hr className="hr-light"/>
+                  <h6 className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellendus quasi fuga nesciunt
+                  dolorum nulla magnam veniam sapiente, fugiat! Commodi sequi non animi ea dolor molestiae
+                  iste.</h6>
+                  <Button color="white">Download</Button>
+                  <Button outline color="white">Learn More</Button>
+                </div>
+                <Col md="6" xl="5" className="mt-xl-5">
+                  <img src="https://mdbootstrap.com/img/Mockups/Transparent/Small/admin-new.png" className="img-fluid"/>
+                </Col>
+                </Row>
+              </Container>
+            </Mask>
+          </View>
+
+          <Container fluid>
+            <Row className="py-5">
+              <Col md="12" className="text-center">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </Col>
+            </Row>
+
+            <Footer />
+          </Container>
       </div>
     );
   }
-}
+};
 
-export default App;
+export default AppPage;
---
+++ src/Components/Home/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class home extends Component {
+  render() {
+    return (
+     <h2> Header Header... </h2>
+    );
+  }
+}
+
+export default home;
