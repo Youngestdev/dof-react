5f0bb8fd8909dbe117e89d5a5dd6788aa2d16fc5
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-07-01 23:12:12 +0100

    grouped images here.

---
+++ src/Components/Layout/Images.js
@@ -0,0 +1,5 @@
+export const Fload = require("../../images/desk.png");
+export const House = require("../../images/house.png");
+export const Heart = require("../../images/hearts.svg");
+export const dev = require("../../images/dev.png");
+export const artboard = require("../../images/artboard.png");
